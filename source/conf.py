8ca9eb8d51db0bf9111bb686877eeac8ecd3d2a6
rickmcgeer <rick@mcgeer.com>
2021-05-09 07:55:47 -0700

    completed first pass on all docs

--- source/conf.py
+++ source/conf.py
@@ -58,3 +58,9 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+import sys
+# pull the galyleo client and set the root directory in the environment, e.g.
+# export GALYLEO_CLIENT=/Users/rick/Projects/engageLively/el-galyleo-client
+import os
+sys.path.append(os.environ["GALYLEO_CLIENT"])
